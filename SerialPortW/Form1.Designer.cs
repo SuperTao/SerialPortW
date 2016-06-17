b9441710a73e075be530780d7f74389cfa294de9
zengjf <zengjf42@163.com>
2016-06-17 14:00:55 +0800

    add c# code to repository.

---
+++ SerialPortW/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace SerialPortW
+{
+    partial class SerialPortW
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.serialPorts = new System.Windows.Forms.ComboBox();
+            this.send = new System.Windows.Forms.Button();
+            this.receive = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Serial Port:";
+            // 
+            // serialPorts
+            // 
+            this.serialPorts.FormattingEnabled = true;
+            this.serialPorts.Location = new System.Drawing.Point(94, 12);
+            this.serialPorts.Name = "serialPorts";
+            this.serialPorts.Size = new System.Drawing.Size(69, 20);
+            this.serialPorts.TabIndex = 1;
+            // 
+            // send
+            // 
+            this.send.Location = new System.Drawing.Point(220, 10);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(42, 23);
+            this.send.TabIndex = 2;
+            this.send.Text = "Send";
+            this.send.UseVisualStyleBackColor = true;
+            this.send.Click += new System.EventHandler(this.send_Click);
+            // 
+            // receive
+            // 
+            this.receive.Location = new System.Drawing.Point(12, 45);
+            this.receive.Multiline = true;
+            this.receive.Name = "receive";
+            this.receive.Size = new System.Drawing.Size(250, 99);
+            this.receive.TabIndex = 3;
+            // 
+            // SerialPortW
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 155);
+            this.Controls.Add(this.receive);
+            this.Controls.Add(this.send);
+            this.Controls.Add(this.serialPorts);
+            this.Controls.Add(this.label1);
+            this.Name = "SerialPortW";
+            this.Text = "SerialPortW";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox serialPorts;
+        private System.Windows.Forms.Button send;
+        private System.Windows.Forms.TextBox receive;
+
+    }
+}
+
---
+++ SerialPortW/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SerialPortW
+{
+    public partial class SerialPortW : Form
+    {
+        public SerialPortW()
+        {
+            InitializeComponent();
+
+            String[] serialPortString = SerialPort.GetPortNames();
+            ComboBox serialPorts = (ComboBox)GetControl("serialPorts", this);
+            if (serialPortString != null && serialPortString.Length != 0 && serialPorts != null)
+            {
+                Array.Sort<String>(serialPortString);
+                foreach (String port in serialPortString)
+                {
+                    if (port != null && port.Length != 0)
+                        serialPorts.Items.Add(port);
+                }
+                serialPorts.SelectedIndex = 0;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private Control GetControl(string name, Control parent)
+        {
+            foreach (Control Item in parent.Controls)
+            {
+                if (Item.Name == name)
+                {
+                    return Item;
+                }
+                else
+                {
+                    Control control = GetControl(name, Item);
+                    if (control != null)
+                    {
+                        return control;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private void send_Click(object sender, EventArgs e)
+        {
+            SerialPort serialPort = new SerialPort();
+
+            serialPort.PortName = (String)((ComboBox)GetControl("serialPorts", this)).SelectedItem;
+            serialPort.BaudRate = 115200;
+            serialPort.DataBits = 8;
+            serialPort.StopBits = StopBits.One;
+            serialPort.Parity = Parity.Even;
+
+            serialPort.Open();
+            serialPort.Write("FE FE FE 68 AA AA AA AA AA AA 68 13 00 \n");
+
+            Thread.Sleep(100);
+            String data = serialPort.ReadExisting();
+
+            serialPort.Close();
+
+            TextBox receive = (TextBox)GetControl("receive", this);
+            receive.AppendText(data);
+        }
+    }
+}
