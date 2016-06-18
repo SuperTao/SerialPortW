5169a370d7bd82447779a01972a74d0b5b71c196
zengjf <zengjf42@163.com>
2016-06-18 15:55:55 +0800

    add more function to demo.

--- SerialPortW/Form1.Designer.cs
+++ SerialPortW/Form1.Designer.cs
@@ -32,12 +32,19 @@
             this.serialPorts = new System.Windows.Forms.ComboBox();
             this.send = new System.Windows.Forms.Button();
             this.receive = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.baudRate = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.parity = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.stop = new System.Windows.Forms.ComboBox();
+            this.start = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(11, 15);
+            this.label1.Location = new System.Drawing.Point(12, 14);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(77, 12);
             this.label1.TabIndex = 0;
@@ -46,16 +53,16 @@
             // serialPorts
             // 
             this.serialPorts.FormattingEnabled = true;
-            this.serialPorts.Location = new System.Drawing.Point(94, 12);
+            this.serialPorts.Location = new System.Drawing.Point(94, 11);
             this.serialPorts.Name = "serialPorts";
-            this.serialPorts.Size = new System.Drawing.Size(69, 20);
+            this.serialPorts.Size = new System.Drawing.Size(84, 20);
             this.serialPorts.TabIndex = 1;
             // 
             // send
             // 
-            this.send.Location = new System.Drawing.Point(220, 10);
+            this.send.Location = new System.Drawing.Point(185, 14);
             this.send.Name = "send";
-            this.send.Size = new System.Drawing.Size(42, 23);
+            this.send.Size = new System.Drawing.Size(78, 41);
             this.send.TabIndex = 2;
             this.send.Text = "Send";
             this.send.UseVisualStyleBackColor = true;
@@ -63,24 +70,128 @@
             // 
             // receive
             // 
-            this.receive.Location = new System.Drawing.Point(12, 45);
+            this.receive.Location = new System.Drawing.Point(12, 117);
             this.receive.Multiline = true;
             this.receive.Name = "receive";
             this.receive.Size = new System.Drawing.Size(250, 99);
             this.receive.TabIndex = 3;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Baud Rate:";
+            // 
+            // baudRate
+            // 
+            this.baudRate.FormattingEnabled = true;
+            this.baudRate.Items.AddRange(new object[] {
+            "50",
+            "75",
+            "110",
+            "134",
+            "150",
+            "200",
+            "300",
+            "600",
+            "1200",
+            "1800",
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200",
+            "230400",
+            "460800",
+            "500000",
+            "576000",
+            "921600",
+            "1000000",
+            "1152000",
+            "1500000",
+            "2000000",
+            "2500000",
+            "3000000",
+            "3500000",
+            "4000000"});
+            this.baudRate.Location = new System.Drawing.Point(94, 35);
+            this.baudRate.Name = "baudRate";
+            this.baudRate.Size = new System.Drawing.Size(84, 20);
+            this.baudRate.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Parity:";
+            // 
+            // parity
+            // 
+            this.parity.FormattingEnabled = true;
+            this.parity.Items.AddRange(new object[] {
+            "EVEN",
+            "ODD"});
+            this.parity.Location = new System.Drawing.Point(94, 59);
+            this.parity.Name = "parity";
+            this.parity.Size = new System.Drawing.Size(84, 20);
+            this.parity.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(54, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Stop:";
+            // 
+            // stop
+            // 
+            this.stop.FormattingEnabled = true;
+            this.stop.Items.AddRange(new object[] {
+            "1",
+            "2"});
+            this.stop.Location = new System.Drawing.Point(94, 82);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(84, 20);
+            this.stop.TabIndex = 7;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(185, 62);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(78, 40);
+            this.start.TabIndex = 9;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
             // SerialPortW
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(274, 155);
+            this.ClientSize = new System.Drawing.Size(274, 228);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.parity);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.baudRate);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.receive);
             this.Controls.Add(this.send);
             this.Controls.Add(this.serialPorts);
             this.Controls.Add(this.label1);
             this.Name = "SerialPortW";
             this.Text = "SerialPortW";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -92,6 +203,13 @@
         private System.Windows.Forms.ComboBox serialPorts;
         private System.Windows.Forms.Button send;
         private System.Windows.Forms.TextBox receive;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox baudRate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox parity;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox stop;
+        private System.Windows.Forms.Button start;
 
     }
 }
--- SerialPortW/Form1.cs
+++ SerialPortW/Form1.cs
@@ -12,70 +12,87 @@ namespace SerialPortW
 {
     public partial class SerialPortW : Form
     {
+
+        SerialPort serialPort;
+
         public SerialPortW()
         {
             InitializeComponent();
 
-            String[] serialPortString = SerialPort.GetPortNames();
-            ComboBox serialPorts = (ComboBox)GetControl("serialPorts", this);
-            if (serialPortString != null && serialPortString.Length != 0 && serialPorts != null)
+            Control.CheckForIllegalCrossThreadCalls = false;
+
+            String[] serialPortArray = SerialPort.GetPortNames();
+            if (serialPortArray != null && serialPortArray.Length != 0)
             {
-                Array.Sort<String>(serialPortString);
-                foreach (String port in serialPortString)
+                Array.Sort<String>(serialPortArray);
+                foreach (String port in serialPortArray)
                 {
                     if (port != null && port.Length != 0)
                         serialPorts.Items.Add(port);
                 }
-                serialPorts.SelectedIndex = 0;
             }
 
+            serialPorts.SelectedIndex = 0;
+            baudRate.SelectedIndex = 16;
+            parity.SelectedIndex = 0;
+            stop.SelectedIndex = 0;
+
+            send.BackColor = Color.Gray;
+
+            serialPort = new SerialPort();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void dealReceive(object sender, SerialDataReceivedEventArgs e)
         {
-
-        }
-
-        private Control GetControl(string name, Control parent)
-        {
-            foreach (Control Item in parent.Controls)
-            {
-                if (Item.Name == name)
-                {
-                    return Item;
-                }
-                else
-                {
-                    Control control = GetControl(name, Item);
-                    if (control != null)
-                    {
-                        return control;
-                    }
-                }
-            }
-            return null;
+            String data = serialPort.ReadExisting();
+            receive.AppendText(data);
         }
 
         private void send_Click(object sender, EventArgs e)
         {
-            SerialPort serialPort = new SerialPort();
+            if (serialPort != null && serialPort.IsOpen)
+                serialPort.Write("FE FE FE 68 AA AA AA AA AA AA 68 13 00 \n");
 
-            serialPort.PortName = (String)((ComboBox)GetControl("serialPorts", this)).SelectedItem;
-            serialPort.BaudRate = 115200;
-            serialPort.DataBits = 8;
-            serialPort.StopBits = StopBits.One;
-            serialPort.Parity = Parity.Even;
+        }
 
-            serialPort.Open();
-            serialPort.Write("FE FE FE 68 AA AA AA AA AA AA 68 13 00 \n");
+        private void setPort()
+        {
+            try
+            {
+                serialPort.PortName = (String)(serialPorts.SelectedItem);
+                serialPort.BaudRate = int.Parse((String)(baudRate.SelectedItem));
+                serialPort.DataBits = 8;
+                serialPort.StopBits = int.Parse((String)(baudRate.SelectedItem)) == 0 ? StopBits.One : StopBits.Two;
+                serialPort.Parity = int.Parse((String)(baudRate.SelectedItem)) == 0 ? Parity.Even : Parity.Odd;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
 
-            Thread.Sleep(100);
-            String data = serialPort.ReadExisting();
+        private void start_Click(object sender, EventArgs e)
+        {
+            if (start.Text == "Start")
+            {
+                if (serialPort.IsOpen)
+                    serialPort.Close();
 
-            serialPort.Close();
+                setPort();
+                serialPort.DataReceived += new SerialDataReceivedEventHandler(dealReceive) ;
 
-            TextBox receive = (TextBox)GetControl("receive", this);
-            receive.AppendText(data);
+                serialPort.Open();
+
+                start.Text = "Stop";
+                send.BackColor = Color.GreenYellow;
+            }
+            else
+            {
+                serialPort.Close();
+
+                start.Text = "Start";
+                send.BackColor = Color.Gray;
+            }
         }
     }
 }
